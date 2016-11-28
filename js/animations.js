fc55605de9d2cf3070ef008bbb8c2f017d09539a
Arthur Revaclier <a.r.revaclier@student.tue.nl>
2016-11-28 17:45:46 +0100

    More and more design

--- js/animations.js
+++ js/animations.js
@@ -6,52 +6,43 @@ $(document).ready(function(){
 
     //Dropdown menu
     $('.dropdown').on('show.bs.dropdown', function(e){
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(200);
     });
     $('.dropdown').on('hide.bs.dropdown', function(e){
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-    });
-
-    //Tiles height
-    $(window).resize(function() {
-        $('.dash-unit').each(function () {
-            $(this).height($(this).width());
-        });
-    });
-
-    $('.dash-unit').each(function () {
-        $(this).height($(this).width());
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
     });
 
     //Hidden well
     $('.navigation-well').hide();
 
     $('.light-tile').click(function () {
-        $('.switch-tile').toggle(400);
-       $('.torrent-tile').toggle(400);
-       $('.media-tile').toggle(400);
-       $('.light-well').toggle(400);
+
+       $('.switch-tile').toggle(200);
+       $('.torrent-tile').toggle(200);
+       $('.media-tile').toggle(200);
+       $('.light-well').fadeToggle(200);
     });
 
     $('.switch-tile').click(function () {
-        $('.light-tile').toggle(400);
-        $('.torrent-tile').toggle(400);
-        $('.media-tile').toggle(400);
-        $('.switch-well').toggle(400);
+
+        $('.light-tile').toggle(200);
+        $('.torrent-tile').toggle(200);
+        $('.media-tile').toggle(200);
+        $('.switch-well').fadeToggle(200);
     });
 
     $('.torrent-tile').click(function () {
-        $('.switch-tile').toggle(400);
-        $('.light-tile').toggle(400);
-        $('.media-tile').toggle(400);
-        $('.torrent-well').toggle(400);
+        $('.light-tile').toggle(200);
+        $('.switch-tile').toggle(200);
+        $('.media-tile').toggle(200);
+        $('.torrent-well').fadeToggle(200);
     });
 
     $('.media-tile').click(function () {
-        $('.switch-tile').toggle(400);
-        $('.torrent-tile').toggle(400);
-        $('.light-tile').toggle(400);
-        $('.media-well').toggle(400);
+        $('.light-tile').toggle(200);
+        $('.torrent-tile').toggle(200);
+        $('.switch-tile').toggle(200);
+        $('.media-well').fadeToggle(200);
     });
 });
 

d02df8196def1a6f6fc83875a6a474a23be3a20b
Arthur Revaclier <a.r.revaclier@student.tue.nl>
2016-12-03 21:14:48 +0100

    work on lights.html, including new buttons and animations

--- js/behaviour_web.js
+++ js/behaviour_web.js
@@ -2,6 +2,8 @@
  * Created by arthur on 03/12/16.
  */
 $(document).ready(function () {
+
+    //Home page
     $('.lamp-group').click(function () {
         if($(this).hasClass("light-room-off")) {
             $(this).toggleClass('light-room-off light-room-on');
@@ -21,6 +23,42 @@ $(document).ready(function () {
             $(this).toggleClass("switch-on switch-off");
             $(this).find(".state").text("OFF");
         }
-    })
+    });
+
+    //Lights page
+
+    //All on / all off buttons
+    $('.all-button').click(function () {
+        if($(this).hasClass("all-on-1")) {
+            $('.room-1').removeClass("light-room-off");
+            $('.room-1').addClass("light-room-on");
+            $('.room-1-text').text("ON");
+        }
+        else if($(this).hasClass("all-off-1")) {
+            $('.room-1').removeClass("light-room-on");
+            $('.room-1').addClass("light-room-off");
+            $('.room-1-text').text("OFF");
+        }
+        else if($(this).hasClass("all-on-2")) {
+            $('.room-2').removeClass("light-room-off");
+            $('.room-2').addClass("light-room-on");
+            $('.room-2-text').text("ON");
+        }
+        else if($(this).hasClass("all-off-2")) {
+            $('.room-2').removeClass("light-room-on");
+            $('.room-2').addClass("light-room-off");
+            $('.room-2-text').text("OFF");
+        }
+        else if($(this).hasClass("all-on-3")) {
+            $('.room-3').removeClass("light-room-off");
+            $('.room-3').addClass("light-room-on");
+            $('.room-3-text').text("ON");
+        }
+        else if($(this).hasClass("all-off-3")) {
+            $('.room-3').removeClass("light-room-on");
+            $('.room-3').addClass("light-room-off");
+            $('.room-3-text').text("OFF");
+        }
+    });
 
 });
